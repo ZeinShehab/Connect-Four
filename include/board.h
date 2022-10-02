a7604e5ed50f75961ed8a4609c84ef8a9873d51f
Zein Shehab <zeinshehab@outlook.com>
2022-10-02 16:56:56 +0300

    Added an extra row to track num of pieces per column

--- include/board.h
+++ include/board.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#define BOARD_HEIGHT 6
+#define BOARD_HEIGHT 7     // One extra row to track number of pieces in each column
 #define BOARD_WIDTH 7
 
 int get(int i, int j, int* board);
@@ -10,3 +10,9 @@ void set(int i, int j, int value, int* board);
 void reset(int* board);
 
 void show(int* board);
+
+int numPiecesInCol(int col, int* board);
+
+int isFullColumn(int col, int* board);
+
+int nextEmptyRow(int col, int* board);
--- src/board.c
+++ src/board.c
@@ -29,11 +29,37 @@ void hozLine()
 void show(int* board)
 {
 	hozLine();
-	for (int i = 0; i < BOARD_HEIGHT; i++) {
+	for (int i = 0; i < BOARD_HEIGHT-1; i++) {
 		for (int j = 0; j < BOARD_WIDTH; j++) {
 			printf("|%d|", get(i, j, board));
 		}
 		printf("\n");
 	}
 	hozLine();
+
+	// -- FOR DEBUGGING REMOVE LATER -- //
+	for (int i = 0; i < BOARD_WIDTH; i++) {
+		printf("|%d|", get(BOARD_HEIGHT - 1, i, board));
+	}
+	printf("\n");
+	hozLine();
+	// -- FOR DEBUGGING REMOVE LATER -- //
+}
+
+int numPiecesInCol(int col, int* board)
+{
+	return get(BOARD_HEIGHT - 1, col, board);
+}
+
+int isFullColumn(int col, int* board)
+{
+	if (numPiecesInCol(col, board) == BOARD_HEIGHT - 1) {
+		return 1;
+	}
+	return 0;
+}
+
+int nextEmptyRow(int col, int* board)
+{
+	return BOARD_HEIGHT - 2 - numPiecesInCol(col, board);
 }
