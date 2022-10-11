45051543b052e533156460ba048937d9e3ec242b
Zein Shehab <zeinshehab@outlook.com>
2022-10-11 15:41:44 +0300

    Added fucntions for console operations

---
+++ include/console.h
@@ -0,0 +1,25 @@
+#pragma once
+
+void resetColor();
+
+void printRed(char *string);
+
+void printIntRed(char *string, int value);
+
+void printGreen(char *string);
+
+void printIntGreen(char *string, int value);
+
+void printYellow(char* string);
+
+void printIntYellow(char* string, int value);
+
+void printBlue(char* string);
+
+int consoleWidth();
+
+int consoleHeight();
+
+void centerLine(int textLength);
+
+void clrscr();
